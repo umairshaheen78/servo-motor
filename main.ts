c2b384601da8c63601f10b8e7d1e4bdf7368bf49
Umair Shaheen <umairshaheen7@gmail.com>
2022-09-30 15:30:29 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,18 @@ input.onButtonPressed(Button.A, function () {
         . . # . .
         `)
     basic.clearScreen()
+    pins.servoWritePin(AnalogPin.P0, 90)
+    pins.servoWritePin(AnalogPin.P0, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . # . # .
+        . . # . .
+        `)
+    basic.clearScreen()
     pins.servoWritePin(AnalogPin.P0, 180)
     pins.servoWritePin(AnalogPin.P0, 90)
 })
